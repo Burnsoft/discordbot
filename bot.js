c100c6ffe184d6547dc3bf7b7cb96f71108b4128
Burnsoft <nik@burnsoft.co.uk>
2018-02-04 09:17:06 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,8 +12,8 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
+    if (message.content === 'pizza') {
+    	message.channel.send('and i better get it too');
   	}
 });
 
